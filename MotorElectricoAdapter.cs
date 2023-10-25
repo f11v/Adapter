29345cff713399713c77e4cd5f5627d03edad353
f11v <146761375+f11v@users.noreply.github.com>
2023-10-25 02:26:00 -0500

    todo

---
+++ MotorElectricoAdapter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Adapter
+{
+    public class MotorElectricoAdapter : Motor
+    {
+        private MotorElectrico motorElectrico;
+
+        public MotorElectricoAdapter()
+        {
+            motorElectrico = new MotorElectrico();
+        }
+
+        public override string Acelerar()
+        {
+            return motorElectrico.Mover();
+        }
+
+        public override string Arrancar()
+        {
+            string resultado = motorElectrico.Conectar();
+            resultado += "\n" + motorElectrico.Activar();
+            return resultado;
+        }
+
+        public override string CargarCombustible()
+        {
+            return motorElectrico.Enchufar();
+        }
+
+        public override string Detener()
+        {
+            string resultado = motorElectrico.Parar();
+            resultado += "\n" + motorElectrico.Desactivar();
+            resultado += "\n" + motorElectrico.Desconectar();
+            return resultado;
+        }
+    }
+}
+
+
